2887c92b8f5e29813c2c76d8facbf5312f4d1821
alvindioprakosa <alvindioprakosa@gmail.com>
2025-04-14 11:43:46 +0700

    Update Controller Tag

--- app/tag/model.js
+++ app/tag/model.js
@@ -9,17 +9,19 @@ const tagSchema = new Schema({
   name: {
     type: String,
     trim: true,
-    minlength: [3, 'Panjang nama kategori minimal 3 karakter'],
-    maxlength: [20, 'Panjang nama kategori maksimal 20 karakter'],
-    required: [true, 'Nama kategori harus diisi'],
+    minlength: [3, 'Panjang nama tag minimal 3 karakter'],
+    maxlength: [20, 'Panjang nama tag maksimal 20 karakter'],
+    required: [true, 'Nama tag harus diisi'],
     unique: true
   }
-}, { timestamps: true });
+}, {
+  timestamps: true // Menyimpan createdAt dan updatedAt
+});
 
 // (4) Middleware untuk menangani duplicate key error
 tagSchema.post('save', function(error, doc, next) {
   if (error.name === 'MongoServerError' && error.code === 11000) {
-    next(new Error('Nama kategori sudah digunakan, silakan pilih nama lain.'));
+    next(new Error('Nama tag sudah digunakan, silakan pilih nama lain.'));
   } else {
     next(error);
   }
